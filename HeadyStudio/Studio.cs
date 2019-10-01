4261f58ae5c20adf0ccc838e06dca59d25e27eae
MirandaRose <55806378+MirandaRose@users.noreply.github.com>
2019-10-01 08:05:05 -0700

    Studio Name and Address

---
+++ HeadyStudio/Studio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeadyStudio
+{
+    class Studio
+    {
+        public string StudioName { get; set; }
+
+        public string StudioAddress { get; set; }
+    }
+}
