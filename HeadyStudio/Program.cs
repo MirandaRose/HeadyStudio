c949430a33547735464a30c58b5d4c17c15a4e00
MirandaRose <55806378+MirandaRose@users.noreply.github.com>
2019-09-30 23:56:51 -0700

    Initial commit

---
+++ HeadyStudio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HeadyStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instance of an account == objext
+            var UserAccount = new UserAccount
+            {
+                EmailAddress = "test@test.com"
+            };
+
+            UserAccount.Deposit3mm(22);
+            UserAccount.Deposit14mm(5);
+            UserAccount.DepositAqua(4);
+
+            Console.WriteLine($"EmailAddress: {UserAccount.EmailAddress}, " +
+                $"Account Number: {UserAccount.AccountNumber}, " +
+                $"CreatedDate: {UserAccount.DateCreated}, " +
+                $"Clear Rod 3mm: {UserAccount.ClearRod3mm}, " +
+                $"Clear Rod 14mm: {UserAccount.ClearRod14mm}, " +
+                $"Aqua Azul Rod: {UserAccount.AquaAzulRod}");
+
+            var UserAccount2 = new UserAccount();
+
+
+
+        }
+    }
+}
---
+++ HeadyStudio/UserAccount.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeadyStudio
+{
+    class UserAccount
+    {
+        private static int LastUserAccountNumber = 0;
+
+        #region UserAccountProperties
+        /// <summary>
+        /// Email address of the user account
+        /// </summary>
+        public string EmailAddress { get; set; }
+        /// <summary>
+        /// Autogenrated number of the user account
+        /// </summary>
+        public int AccountNumber { get; }
+        /// <summary>
+        /// Date the user created account
+        /// </summary>
+        public DateTime DateCreated { get; }
+        /// <summary>
+        /// Quantity of 3mm clear rods
+        /// </summary>
+        public int ClearRod3mm { get; set; }
+        /// <summary>
+        /// Quantity of 14mm clear rods
+        /// </summary>
+
+        public int ClearRod14mm { get; set; }
+        /// <summary>
+        /// Quantity of AquaAzul rods
+        /// </summary>
+        public int AquaAzulRod { get; set; }
+        #endregion
+
+        #region Contrustor
+        public UserAccount()
+        {
+            AccountNumber = ++LastUserAccountNumber;
+            DateCreated = DateTime.Now;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Deposit3mm (int amount)
+        {
+            ClearRod3mm +=amount;
+        }
+
+        public void Deposit14mm (int amount)
+        {
+            ClearRod14mm += amount;
+        }
+
+        public void DepositAqua (int amount)
+        {
+            AquaAzulRod += amount;
+        }
+
+        #endregion
+    }
+}
